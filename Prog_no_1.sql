a18c40f2e24795c474a20c72048d1493e3a24e0b
Rakesh B N <68104336+Rakesh-1-7@users.noreply.github.com>
2022-06-20 11:32:31 +0530

    Update Prog_no_1.sql

--- Prog_no_1.sql
+++ Prog_no_1.sql
@@ -25,8 +25,6 @@ insert into car values('KA041702','Lamborgini','2005');
 select * from car;
 
 
-
-drop table accident;
 create table accident(report_num int, acc_date date, location varchar(30), primary key(report_num));
 
 insert into accident values(11,'2008-12-10','Delhi');
@@ -38,7 +36,21 @@ insert into accident values(15,'2012-9-30','Mysore');
 select * from accident;
 
 create table OWNS ( d_id varchar(20), Regno varchar(20), primary key(d_id,Regno), foreign key(d_id) references person(driver_id) );
+insert into OWNS VALUES('A01','KA052250');
+insert into OWNS VALUES('A02','KA053408');
+insert into OWNS VALUES('A03','KA031181');
+insert into OWNS VALUES('A04','KA095477');
+insert into OWNS VALUES('A05','KA041702');
 
-desc OWNS;
 
 create table PARTICIPATED(driv_id varchar(20), Regno varchar(20), report_number int, damage_amount int, primary key(driv_id,Regno,report_number));
+
+insert into PARTICIPATED values('A01','KA052250',11,10000);
+insert into PARTICIPATED values('A02','KA053408',12,50000);
+insert into PARTICIPATED values('A03','KA031181',13,25000);
+insert into PARTICIPATED values('A04','KA095477',14,3000);
+insert into PARTICIPATED values('A05','KA041702',15,5000);
+commit;
+SELECT * FROM PARTICIPATED;
+
+select count(driv_id) from participated where driv_id="A04";
